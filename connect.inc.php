fe7efe6b42d70799fd3646a631e11045a75d9230
andreask7 <andreask7@users.noreply.github.com>
2016-02-12 11:44:55 +0100

    added connect.inc.php for openshift

---
+++ connect.inc.php
@@ -0,0 +1,6 @@
+<?php
+$server = getenv('OPENSHIFT_MYSQL_DB_HOST').":".getenv('OPENSHIFT_MYSQL_DB_PORT');
+$userid = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$passwd = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbname = getenv('OPENSHIFT_GEAR_NAME');
+?>
